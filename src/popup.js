13f2cee9c794a26dd43adbef8fab319c1d84cb2c
pedrinholemes <pedroca21265@gmail.com>
2020-09-14 19:46:39 -0300

    :sparkles: Add buttons for google/brainly search

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,14 @@
 const input = document.querySelector('input[name="question"]')
-const button = document.querySelector('button')
-input.addEventListener('change', function () {
-  if (this.value !== '') button.disabled = false
-  else button.disabled = true
-})
-input.addEventListener('input', function () {
-  if (this.value !== '') button.disabled = false
-  else button.disabled = true
-})
-button.addEventListener('click', function () {
+const buttons = {
+  brainly: document.querySelector('button#brainly'),
+  google: document.querySelector('button#google')
+}
+
+buttons.brainly.addEventListener('click', function () {
   window.open(`https://brainly.com.br/app/ask?q=${encodeURI(input.value)}`)
 })
+buttons.google.addEventListener('click', function () {
+  window.open(`https://google.com/search?q=${encodeURI(input.value)}`)
+})
+
+document.querySelector('form').addEventListener('submit', e => e.preventDefault())
